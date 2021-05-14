54f5f6a5c36e9cb93008ebc6b7e6b7ad922f94ae
netkawai <netkawai@gmail.com>
2021-05-14 12:40:31 -0400

    Clean up and move Windows Sandbox configuration

--- Windows-Sandbox/Sandbox-logon.cmd
+++ Windows-Sandbox/Sandbox-logon.cmd
@@ -1,10 +1,7 @@
-REM iRing Aegis folder
 
 REM Install virtual printer
 rundll32 printui.dll,PrintUIEntry /if  /f %windir%\inf\ntprint.inf /r "lpt1:" /m "Microsoft MS-XPS Class Driver 2"
 
-
-XCOPY /E /I "C:\Users\WDAGUtilityAccount\Desktop\iBlast\Home\iRing Aegis" "C:\Users\WDAGUtilityAccount\Documents\iRing Aegis"
 
 
 exit
