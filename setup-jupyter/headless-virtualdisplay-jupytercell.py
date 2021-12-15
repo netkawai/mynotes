dc47910a738e867b99f36d4aeaa5fc69d895fb09
netkawai <netkawai@gmail.com>
2021-12-15 15:57:46 -0500

    Move all python codes to different repo

--- setup-jupyter/headless-virtualdisplay-jupytercell.py
+++
@@ -1,3 +0,0 @@
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(600, 400))
-display.start()
--- setup-jupyter/interactive_for_ipywidgets.py
+++
@@ -1,5 +0,0 @@
-from ipywidgets import interact
-
-@interact(x=(0, 100, 10))
-def p(x=50):
-    pass
